cb660041623603227a330af45f03116693078816
Ashwath S <miniash123@outlook.com>
2025-12-06 18:01:07 +0530

    updates contact form to collect data

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
 
 # USE YOUR OWN npoint LINK! ADD AN IMAGE URL FOR YOUR POST. 👇
-posts = requests.get("https://api.npoint.io/c790b4d5cab58020d391").json()
+posts = requests.get("https://api.npoint.io/00b23b65247a891e7d9e").json()
 
 app = Flask(__name__)
 
@@ -30,6 +30,16 @@ def show_post(index):
             requested_post = blog_post
     return render_template("post.html", post=requested_post)
 
+@app.route("/form-entry", methods=["POST"])
+def receive_data():
+    form_data = request.form
+
+    print(form_data.get("name"))
+    print(form_data.get("email"))
+    print(form_data.get("phone"))
+    print(form_data.get("message"))
+
+    return "<h1>Successfully sent your message</h1>"
 
 if __name__ == "__main__":
     app.run(debug=True, port=5001)
